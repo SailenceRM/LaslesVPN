d212c25f33ed01da276f347f71878ade703ffac9
SailenceRM <69628955+SailenceRM@users.noreply.github.com>
2020-08-17 13:34:23 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$('.header__burger').click(function(event){
-		$('.header__burger,.header__nav').toggleClass('active');
+		$('.header__burger,.header__nav,.header__sign').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
 });
